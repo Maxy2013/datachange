db91e7de050825046f2dbed9cd12268c6df3b041
dr_liuzheng <liu.zh_neu@neusoft.com>
2018-06-25 14:36:56 +0800

    添加Ajax提交方式

--- src/main/java/com/data/datachange/module/form/web/FormController.java
+++ src/main/java/com/data/datachange/module/form/web/FormController.java
@@ -5,6 +5,8 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author lz
  * 2018/6/22 17:36
@@ -13,14 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/form")
 public class FormController {
 
-    @RequestMapping("/submitUserInfo")
-    public User submitUserInfo(HttpServletRequest request){
+    @RequestMapping("/submitAjaxUserInfo")
+    public User submitAjaxUserInfo(HttpServletRequest request){
 
+        return this.getUser(request);
+    }
+
+    @RequestMapping("/submitFormUserInfo")
+    public User submitFormUserInfo(HttpServletRequest request){
+
+        return this.getUser(request);
+    }
+
+    private User getUser(HttpServletRequest request){
         User user = new User();
         user.setName(request.getParameter("name"));
         user.setAddr(request.getParameter("addr"));
         user.setPass(request.getParameter("pass"));
-
         return user;
     }
 }
